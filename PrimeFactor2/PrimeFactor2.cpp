1d8a4ebca7df2350a4c76e5f847c47a8c297fa95
yull.kim <yull618@naver.com>
2024-05-17 08:44:05 +0900

    First Commint

---
+++ PrimeFactor2/PrimeFactor2.cpp
@@ -0,0 +1,9 @@
+
+
+class PrimeFactor {
+
+};
+
+int main() {
+
+}
---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+#include "../PrimeFactor2/PrimeFactor2.cpp"
+using namespace std;
+
+TEST(PrimeFator, Of1) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
